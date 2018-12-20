2ea05c6a0ce15ea3445d8ebf59493f000bec35f5
ENVYPS\psubi <psubianto@gmail.com>
2018-12-20 09:40:18 -0500

    Added web project

---
+++ Runtime/PublisherScheduler01Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PublisherScheduler01Web.Startup))]
+namespace PublisherScheduler01Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
