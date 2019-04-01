079fd19f87dbce55397528eea309745b64e7d0d7
Peter Subianto <psubianto@gmail.com>
2019-04-01 13:51:01 -0400

    Created Data project with new models for Code-First approach. Data is stored in Azure.

---
+++ Runtime/PublisherScheduler01Data/Models/Slot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PublisherScheduler01Data.Models
+{
+    public class Slot
+    {
+        public int Id { get; set; }
+        public DateTime DateBegin { get; set; }
+        public DateTime TimeBegin { get; set; }
+        public DateTime DateEnd { get; set; }
+        public DateTime TimeEnd { get; set; }
+        public int TaskId { get; set; }
+        public int LocationId { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ Runtime/PublisherScheduler01Data/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublisherScheduler01Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public virtual ICollection<Role> Roles { get; set; }
+        public virtual ICollection<Slot> Slots { get; set; }
+    }
+}
